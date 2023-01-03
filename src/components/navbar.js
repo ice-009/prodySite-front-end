dcd49b6d6e7c0d2067064c4da9c36fec4e6b24e2
ice-009 <21bma009@nith.ac.in>
2023-01-03 11:48:17 +0530

    navbar added

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {Nav,NavbarContainer, NavLogo} from './navbarelements'
+import { FaBars } from 'react-icons/fa'
+import {Nav,NavbarContainer, NavLogo, NavMenu ,MobileIcon, NavItem, NavLinks, NavBtn, NavBtnLink} from './navbarelements'
 const Navbar = () => {
 return (
 <Nav>
 <NavbarContainer>
-<NavLogo>dolla</NavLogo>
+<NavLogo to='/'>Prody</NavLogo>
+<MobileIcon>
+  <FaBars />
+</MobileIcon>
+<NavMenu>
+<NavItem>
+<NavLinks to='about '>Home</NavLinks>
+</NavItem>
+<NavItem>
+<NavLinks to='timeline '>Timeline</NavLinks>
+</NavItem>
+<NavItem>
+<NavLinks to='sponsors '>Sponsors</NavLinks>
+</NavItem>
+<NavItem>
+<NavLinks to='contact'>Contact Us</NavLinks>
+</NavItem>
+</NavMenu>
+<NavBtn>
+  <NavBtnLink to="/signin">
+    Sign IN
+  </NavBtnLink>
+</NavBtn>
 </NavbarContainer>
 </Nav>
 )};
